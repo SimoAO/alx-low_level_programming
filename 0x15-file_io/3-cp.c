409625e6c228af0d48700fc202ff2aa788e08e94
SimoAO <aitouaazizmohammed@gmail.com>
2023-05-09 09:57:25 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ void print_error(int in, int out, char *argv[])
 {
 	if (in == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't red from file %s\n", argv[1]);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 	}
 
