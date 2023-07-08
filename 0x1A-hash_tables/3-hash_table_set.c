d5c0814d5ee2a5d5be51eabffc005bd550a5854e
simoao <aitouaazizmohammed@gmail.com>
2023-07-07 17:13:49 -0700

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,4 +11,42 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int in = 0;
+	hash_node_t *h = NULL, *n = NULL;
 
+	if (!ht || !key || !*key || !value || !*value)
+	return (0);
+	in = key_index((unsigned char *)key, ht->size);
+	h = ht->array[i];
+	while (h != NULL)
+	{
+	if (strcmp(h->key, key) == NULL)
+	{
+	free(h->value);
+	h->value = strdup((char *)value);
+	if (h->value == NULL)
+	return (0);
+	return (1);
+	}
+	h = h->next;
+	}
+	n = malloc(sizeof(hash_node_t));
+	if (n == NULL)
+	return (0);
+	n->key = strdup((char *)key);
+	if (n->key == NULL)
+	{
+	free(n);
+	return (0);
+	}
+	n->value = stdrup(value);
+	if (n->value == NULL)
+	{
+	free(n);
+	free(n->key);
+	return (0);
+	}
+	n->next = ht->array[in];
+	ht->array[in] = n;
+	return (1);
+}
